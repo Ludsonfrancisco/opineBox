a15675748bb3a3c5458e7eb3dfd7c359c4d588d3
Ludson Francisco <ludsonfrancisco1010@gmail.com>
2021-10-23 15:22:39 -0300

    ajuste form

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -36,32 +36,37 @@ const Pesquisa = () => {
   }
   return (
     <div className='pt-6'>
-      <PageTitle  title='Pesquisea '/>
+
+
+
+      <PageTitle title='Pesquisea ' />
       <h1 className='text-center font-bold mb-4 text-2xl'>Críticas e sugestões</h1>
       <p className='text-center mb-6 text-sm px-2'>O restaurante X sempre busca por atender melhor seus clientes.<br />
         Por isso, estamos sempre abertos a ouvir a sua opinião.</p>
       {!sucess && <div className=' w-4/5 lg:w-1/5 mx-auto'>
-        <label className='font-bold'>Seu nome:</label>
-        <input className='focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' type='text' placeholder='Digite seu Nome' onChange={onChange} name='Nome' value={form.Nome} />
-        <label className='font-bold'>Seu E-mail:</label>
-        <input className='focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' type='text' placeholder='Digite seu E-mail' onChange={onChange} name='Email' value={form.Email} required />
-        <label className='font-bold sm:text-center'>Seu Whatsapp:</label>
-        <input className='focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' type='text' placeholder='Digite seu Whatsapp' onChange={onChange} name='Whatsapp' value={form.Whatsapp} />
-        <label className='font-bold'>Nota:</label>
-        
-        <div className='flex pt-4 pb-6 cursor-pointer focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent '> 
+        <label className='font-bold pp:hidden'>Seu nome:</label>
+        <input className='pp:mb-5 placeholder-gray-900 placeholder-opacity-25 pp:placeholder-opacity-75 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' type='text' placeholder='Digite seu Nome' onChange={onChange} name='Nome' value={form.Nome} />
+        <label className='font-bold pp:hidden'>Seu E-mail:</label>
+        <input className='pp:mb-5 placeholder-gray-900 placeholder-opacity-25 pp:placeholder-opacity-75 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' type='text' placeholder='Digite seu E-mail' onChange={onChange} name='Email' value={form.Email} required />
+        <label className='font-bold pp:hidden'>Seu Whatsapp:</label>
+        <input className='pp:mb-5 placeholder-gray-900 placeholder-opacity-25 pp:placeholder-opacity-75 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' type='text' placeholder='Digite seu Whatsapp' onChange={onChange} name='Whatsapp' value={form.Whatsapp} />
+        <label className='font-bold leading-none tracking-tight	'>Que nota você daria para o <br />estabelecimento:</label>
+
+
+        <div className='flex justify-center pt-4  pb-6 cursor-pointer focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent '>
           {notas.map(nota => {
-            return (<label className='block w-1/6 text-left '>
-                    {nota} <br />
-                    <input type='radio' src='' name='Nota' value={nota} onChange={onChange} />
-                  </label>)
+            return (<label className='block w-8 text-left'>
+              {nota} <br />
+              <input type='radio' src='' name='Nota' value={nota} onChange={onChange} />
+            </label>)
 
           })}
-        </div>
-        <label className='font-bold'>Sua opinião:</label>
-        <textarea className='focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' rows='4' cols='25' type='text' placeholder='Mensagem' onChange={onChange} name='Mensagem' value={form.Mensagem} />
 
-        <button className='text-white w-full mb-6 bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-12 py-5 mt-6 rounded-lg font-bold shadow-xl hover:shadow ' onClick={save}>Salvar</button>
+        </div>
+        <label className='font-bold pp:hidden'>Sua opinião:</label>
+        <textarea className='pp:mb-5 placeholder-gray-900 placeholder-opacity-25 pp:placeholder-opacity-75 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent  w-full p-4 block shadow bg-blue-100 my-2 rounded' rows='4' cols='25' type='text' placeholder='Digite sua opinião' onChange={onChange} name='Mensagem' value={form.Mensagem} />
+
+        <button className='text-white w-full mb-6 bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-12 py-5 mt-6 rounded-lg font-bold shadow-xl hover:shadow ' onClick={save}>Enviar</button>
       </div>}
       {sucess && <div className=' w-4/5 lg:w-1/5 mx-auto'>
         <p className=' mt-6 text-center  border-blue-500 text-blue-700 bg-withe px-4 py-3'> Obrigado por contribuir com sua sujestão ou crítica</p>
@@ -77,7 +82,7 @@ const Pesquisa = () => {
             <br />
             <span className='italic'>Tire um print ou foto desta tela e apresente ao balcão.</span>
           </div>
-        } 
+        }
       </div>}
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'pp': {'min': '350px', 'max': '639px'},
+      
+    },
+    extend: {
+      backgroundImage: {
+        'ruim':"url('/ruim.png')"
+      },
+      backgroundSize:{
+        '1-4': '25%',
+        '1-10': '10%'
+      }
+    },
   },
   variants: {
     extend: {},
